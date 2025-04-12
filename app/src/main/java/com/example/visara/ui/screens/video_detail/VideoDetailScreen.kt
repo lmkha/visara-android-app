17032c61f7fba2c04566ddc12eb7f8adb09466bb
Le Minh Kha <lmkha0201@gmail.com>
2025-04-12 17:33:00 +0700

    modify: remove unnecessary testing text

--- app/src/main/java/com/example/visara/ui/screens/video_detail/VideoDetailScreen.kt
+++ app/src/main/java/com/example/visara/ui/screens/video_detail/VideoDetailScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -110,8 +109,6 @@ fun VideoDetailScreen(
                     VisaraVideoPlayer(
                         videoPlayerManager = videoPlayerManager,
                     )
-                } else {
-                    Text("Kha")
                 }
             }
             // Info
